894ee796f1b327b008de5297bb1847b6f686b80b
PetrickMac <petrickmccarver@outlook.com>
2026-03-11 18:00:42 -0500

    feat: configure remote state backend in Azure Blob Storage

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,2 +1,12 @@
+
 # Remote state configuration
 # Will be configured after storage account is created
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "stpetricktfstate"
+    container_name       = "tfstate"
+    key                  = "adp-dev.terraform.tfstate"
+  }
+}
